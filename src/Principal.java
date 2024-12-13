0929dbc74fa96017a21d6800c816fd4c30b2aa73
Estanislao Torres Campi <estanislaotorres@gmail.com>
2024-12-13 12:20:07 -0300

    contenido Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args) {
+        ConsultaAPI consulta = new ConsultaAPI();
+        Moneda moneda = consulta.buscaMoneda("ARS");
+        System.out.println(moneda);
+    }
 }
