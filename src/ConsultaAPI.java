e12fcfe8973e286808c2037c27530bf219da812c
Estanislao Torres Campi <estanislaotorres@gmail.com>
2024-12-13 12:15:47 -0300

    contenido consulta API

--- src/ConsultaAPI.java
+++ src/ConsultaAPI.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultaAPI {
+
+    Moneda buscaMoneda(String nombreMoneda){
+
+        URI direccion = URI.create(
+                "https://v6.exchangerate-api.com/v6/83dd5e9d41164de5c8675503/latest/" + nombreMoneda);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Gson().fromJson(response.body(),Moneda.class);
+    }
 }
